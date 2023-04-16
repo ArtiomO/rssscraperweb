5a9dce9b8c834684212bb487f355f438e346e873
Artiom O <ovchinnikov.artiom@gmail.com>
2023-04-16 16:31:45 +0200

    cleanup

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './styles.css'
-import reportWebVitals from './reportWebVitals'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import RootLayout from './pages/Root'
 import ErrorPage from './error'
@@ -48,8 +47,3 @@ root.render(
     </AuthProvider>
   </React.StrictMode>
 )
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
